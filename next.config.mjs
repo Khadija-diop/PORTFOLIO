7eab24c81d62b0f4426f1c100d034c320c1bd8ed
Mahmoud Barry <mahmoudbarrysn@gmail.com>
2024-10-14 13:16:30 +0000

    MEP:

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  assetPrefix: "/Portofiolio/",
+  basePath: "/Portofiolio",
+};
 
 export default nextConfig;
