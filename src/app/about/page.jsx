416026bdc64f5e270ee91879576ac968ca7797dd
Khadija <dija5631@gmail.com>
2024-11-03 17:35:03 +0100

    deploy2

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,65 +1,50 @@
-// import React from 'react';
-// import { FaHtml5, FaCss3Alt, FaBootstrap, FaJs, FaReact, FaNodeJs } from 'react-icons/fa';
-// import { SiNextdotjs, SiTailwindcss, SiStyledcomponents } from 'react-icons/si';
-
-// const About = ({ onSkillSelect }) => {
-//   const skills = [
-//     { name: 'HTML', icon: <FaHtml5 className="text-orange-600" /> },
-//     { name: 'CSS', icon: <FaCss3Alt className="text-blue-600" /> },
-//     { name: 'Bootstrap', icon: <FaBootstrap className="text-purple-600" /> },
-//     { name: 'JavaScript', icon: <FaJs className="text-yellow-500" /> },
-//     { name: 'React', icon: <FaReact className="text-blue-400" /> },
-//     { name: 'Next.js', icon: <SiNextdotjs className="text-black" /> },
-//     { name: 'Node.js', icon: <FaNodeJs className="text-green-600" /> },
-//     { name: 'Tailwind CSS', icon: <SiTailwindcss className="text-teal-400" /> },
-//     { name: 'Styled Components', icon: <SiStyledcomponents className="text-pink-500" /> },
-//   ];
-
-//   return (
-//     <div id='about' className="max-w-6xl min-h-screen mx-auto my-10 p-8 bg-gradient-to-r from-indigo-50 to-white shadow-xl rounded-xl">
-//       <h1 className="text-5xl font-extrabold mb-8 text-center text-indigo-700 animate-fade-in">À propos</h1>
-      
-//       <section className="mb-12 animate-fade-in">
-//         <h2 className="text-3xl font-semibold mb-4 text-indigo-600">Parcours</h2>
-//         <p className="text-lg text-center leading-relaxed text-gray-700">
-//         Je suis une passionnée de technologie,  j&#39;ai fait une licence en développement web mobile, 
-//          avec des compétences solides dans divers langages et frameworks.
-//          Mon parcours m'a permis de réaliser plusieurs projets enrichissants qui ont renforcé ma
-//          maîtrise de ces technologies. J'ai également eu l'opportunité de faire un stage en développement web,
-//          une expérience qui a consolidé mes compétences et mon expertise. Actuellement, je me tourne vers le game design,
-//          un domaine qui me fascine profondément, et j'ai hâte d'apprendre et de développer de nouvelles compétences dans
-//         </p>
-//       </section>
-      
-//       <section className="animate-fade-in">
-//         <h2 className="text-3xl font-semibold mb-8 text-indigo-600">Compétences</h2>
-//         <div className="grid grid-cols-2 sm:grid-cols-3 gap-8">
-//           {skills.map((skill) => (
-//             <div
-//               key={skill.name}
-//               className="flex items-center space-x-4 p-6 bg-white rounded-lg shadow-lg transform transition-all duration-300 hover:scale-110 hover:shadow-2xl cursor-pointer"
-//               onClick={() => onSkillSelect(skill.name)}
-//             >
-//               <span className="text-4xl">{skill.icon}</span>
-//               <span className="text-xl font-bold text-gray-800">{skill.name}</span>
-//             </div>
-//           ))}
-//         </div>
-//       </section>
-//     </div>
-//   );
-// };
-
-// export default About;
- 
-
 "use client"
 import React from 'react';
-import { FaHtml5, FaBootstrap, FaJs, FaReact, FaNodeJs, FaGithub } from 'react-icons/fa';
-import { SiNextdotjs, SiTailwindcss, SiStyledcomponents } from 'react-icons/si';
+import { FaHtml5, FaBootstrap, FaJs, FaReact, FaNodeJs, FaGithub, FaUnity  } from 'react-icons/fa';
+import { SiNextdotjs, SiTailwindcss, SiStyledcomponents,SiAdobephotoshop  } from 'react-icons/si';
+
+// Composant pour l'icône C++
+const CppIcon = () => (
+  <svg
+    fill="none"
+    height="24"
+    viewBox="0 0 24 24"
+    width="24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12.207 16.278C11.1241 17.343 9.63879 18 8 18C4.68629 18 2 15.3137 2 12C2 8.68629 4.68629 6 8 6C9.67492 6 11.1896 6.6863 12.278 7.79303L13.6923 6.37878C12.2418 4.91014 10.2272 4 8 4C3.58172 4 0 7.58172 0 12C0 16.4183 3.58172 20 8 20C10.1911 20 12.1764 19.1192 13.6212 17.6923L12.207 16.278Z"
+      fill="currentColor"
+    />
+    <path d="M15 9H13V11H11V13H13V15H15V13H17V11H15V9Z" fill="currentColor" />
+    <path d="M20 9H22V11H24V13H22V15H20V13H18V11H20V9Z" fill="currentColor" />
+  </svg>
+);
+
+// Composant pour l'icône C
+const CIcon = () => (
+  <svg
+    fill="none"
+    height="25"
+    viewBox="0 0 15 15"
+    width="25"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      clipRule="evenodd"
+      d="M7.5 0.421143L14 4.21281V10.7872L7.5 14.5788L1 10.7872V4.21281L7.5 0.421143ZM7.5 4C5.567 4 4 5.567 4 7.5C4 9.433 5.567 11 7.5 11H7.68198C8.6585 11 9.59503 10.6121 10.2855 9.92157L10.3536 9.85355L9.64645 9.14645L9.57843 9.21447C9.07546 9.71744 8.39329 10 7.68198 10H7.5C6.11929 10 5 8.88071 5 7.5C5 6.11929 6.11929 5 7.5 5H7.68198C8.39329 5 9.07546 5.28257 9.57843 5.78553L9.64645 5.85355L10.3536 5.14645L10.2855 5.07843C9.59503 4.38792 8.6585 4 7.68198 4H7.5Z"
+      fill="black"
+      fillRule="evenodd"
+    />
+  </svg>
+);
 
 const About = ({ onSkillSelect }) => {
   const skills = [
+    { name: 'Langage C', icon: <CIcon className="text-black-600"/> },
+    { name: 'Langage C++ ', icon: <CppIcon className="text-blue-600" /> },
+    { name: 'Unity', icon: <FaUnity className="text-black-600" /> },
+    { name: 'Photoshop', icon: <SiAdobephotoshop className="text-blue-600" /> },
     { name: 'HTML', icon: <FaHtml5 className="text-orange-600" /> },
     { name: 'GitHub', icon: <FaGithub className="text-black" /> }, // Remplacement de l'icône CSS par GitHub
     { name: 'Bootstrap', icon: <FaBootstrap className="text-purple-600" /> },
@@ -69,6 +54,7 @@ const About = ({ onSkillSelect }) => {
     { name: 'Node.js', icon: <FaNodeJs className="text-green-600" /> },
     { name: 'Tailwind CSS', icon: <SiTailwindcss className="text-teal-400" /> },
     { name: 'Styled Components', icon: <SiStyledcomponents className="text-pink-500" /> },
+    
   ];
 
   return (
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,119 +1,3 @@
-// "use client";
-
-// import React from 'react';
-// import Image from 'next/image';
-// import Link from 'next/link';
-
-// const buttonClasses = "bg-purple-500 text-white py-4 px-6 rounded-full text-md font-medium shadow-lg hover:bg-purple-600 hover:text-white transition duration-300 inline-block";
-
-// export default function Home() {
-//   return (
-//     <div className="relative min-h-[80vh] bg-gradient-to-b from-[#D2D2EB] to-white flex items-center justify-center overflow-hidden">
-//       <div className="absolute inset-0 z-0">
-//         <Image
-//           src="/iabac.png"
-//           layout="fill"
-//           objectFit="cover"
-//           className=""
-//           alt="Portfolio logo"
-//         />
-//       </div>
-//       <div className="relative z-10 flex items-center justify-between w-full max-w-6xl p-8">
-//         <div className="flex-2 animate-fade-in">
-//           {/* <h1 className="text-5xl font-bold text-blue-900 mb-4 drop-shadow-lg">
-//             Khady Diop <br/> 
-//             Programmeuse 
-//           </h1> */}
-//           <p className="text-lg text-blue-800 mt-4 mb-4 max-w-2xl text-center">
-//             {/* Je suis en Licence Professionnelle Métiers du jeu vidéo:
-//               Level design et Game design. Passionnée par les nouvelles 
-//               technologies, j&#39;ai également fait une licence en développement web mobile.
-//               Grâce à ce parcours, je veux développer une double compétence dans la création d&#39;expériences
-//               interactives immersives et d&#39;applications performantes. Mon objectif est de combiner
-//               ces deux univers pour concevoir des produits innovants qui enrichissent l&#39;expérience utilisateur,
-//               que ce soit dans le domaine du jeu vidéo ou du développement d&#39;applications. <br /> <br /> */}
-//           </p>
-       
-//           <Link href="#projects">
-//             <p className={buttonClasses}>
-//               Voir mes projets
-//             </p>
-//           </Link>
-//         </div>
-//       </div>
-
-//       <style jsx>{`
-//         @keyframes fade-in {
-//           0% { opacity: 0; transform: translateY(20px); }
-//           100% { opacity: 1; transform: translateY(0); }
-//         }
-//         .animate-fade-in {
-//           animation: fade-in 1s ease-in-out forwards;
-//         }
-//         @keyframes slide-up {
-//           0% { opacity: 0; transform: translateY(50px); }
-//           100% { opacity: 1; transform: translateY(0); }
-//         }
-//         .animate-slide-up {
-//           animation: slide-up 1s ease-in-out forwards;
-//         }
-//       `}</style>
-//     </div>
-//   );
-// }
-
-
-// "use client";
-
-// import React from 'react';
-// import Image from 'next/image';
-// import Link from 'next/link';
-
-// const buttonClasses = "bg-purple-500 text-white py-4 px-6 rounded-full text-md font-medium shadow-lg hover:bg-purple-600 hover:text-white transition duration-300 inline-block";
-
-// export default function Home() {
-//   return (
-//     <div className="relative min-h-[80vh] bg-gradient-to-b from-[#D2D2EB] to-white flex items-center justify-center overflow-hidden">
-//       <div className="absolute inset-0 z-0">
-//         <Image
-//           src="/iabac.png"
-//           layout="fill"
-//           objectFit="cover"
-//           className=""
-//           alt="Portfolio logo"
-//         />
-//       </div>
-//       <div className="relative z-0flex flex-col items-center justify-between w-full max-w-6xl ">
-//         <div className="flex justify-center">
-//           <Link href="#projects">
-//             <p className={buttonClasses}>
-//               Voir mes projets
-//             </p>
-//           </Link>
-//         </div>
-//       </div>
-
-//       <style jsx>{`
-//         @keyframes fade-in {
-//           0% { opacity: 0; transform: translateY(20px); }
-//           100% { opacity: 1; transform: translateY(0); }
-//         }
-//         .animate-fade-in {
-//           animation: fade-in 1s ease-in-out forwards;
-//         }
-//         @keyframes slide-up {
-//           0% { opacity: 0; transform: translateY(50px); }
-//           100% { opacity: 1; transform: translateY(0); }
-//         }
-//         .animate-slide-up {
-//           animation: slide-up 1s ease-in-out forwards;
-//         }
-//       `}</style>
-//     </div>
-//   );
-// }
-
-
 "use client";
 
 import React from 'react';
@@ -127,20 +11,20 @@ export default function Home() {
     <div className="relative min-h-[80vh] bg-gradient-to-b from-[#D2D2EB] to-white flex items-center justify-center overflow-hidden">
       <div className="absolute inset-0 z-0">
         <Image
-          src="/clock3.jpg"
+          src="/Pempem.png"
           layout="fill"
-          objectFit="center"
+          objectFit="cover"
           className=""
           alt="Portfolio logo"
         />
       </div>
       <div className="relative z-10 flex flex-col items-center justify-between w-full max-w-6xl p-8">
         <div className="flex justify-center mt-auto">
-          <Link href="#projects">
+          {/* <Link href="#projects">
             <p className={buttonClasses}>
               Voir mes projets
             </p>
-          </Link>
+          </Link> */}
         </div>
       </div>
 
